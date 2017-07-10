1fa788dd6b3713d11494e5780092454fa6ac7b67
Rupesh Mittal <Rupesh@Anishs-MacBook-Pro.local>
2017-07-10 21:08:03 +0530

    finalizwded

--- level/ViewController.swift
+++ level/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
-class ViewController: UIViewController {
-
+    
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
+        
+        if (FBSDKAccessToken.current()) != nil {
+            fetchProfile()
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func fetchProfile() {
+        print("Fetch Profile")
+        let graphRequest:FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"name,email, picture.type(large)"])
+        
+        graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(String(describing: error))")
+                return
+            }
+            if let data = result as? [String:Any] {
+                print(data)
+            }
+        })
+    
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("completed login")
+        fetchProfile()
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+        
+    }
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
